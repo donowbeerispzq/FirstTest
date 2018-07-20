31cf90d57f5806a53290b2dc539213425939b30f
donowbeer <102216213@qq.com>
2018-07-20 15:09:48 +0800

    FirstTest A simple maven-archetype-quickstart project

---
+++ src/main/java/com/donowbeer/App.java
@@ -0,0 +1,18 @@
+package com.donowbeer;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        var list = new ArrayList<String>();
+    }
+
+
+}
